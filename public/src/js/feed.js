bdcfb206429cc0ecd58bbc891f45ced5982646cc
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-01-28 20:22:13 +0000

    add button

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -30,6 +30,10 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
+function onSaveButtonClick(event) {
+    console.log('Clicked');
+}
+
 function createCard() {
     var cardWrapper = document.createElement('div');
     cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
@@ -48,6 +52,10 @@ function createCard() {
     cardSupportingText.className = 'mdl-card__supporting-text';
     cardSupportingText.textContent = 'In San Francisco';
     cardSupportingText.style.textAlign = 'center';
+    var cardSaveButton = document.createElement('button');
+    cardSaveButton.textContent = 'Save';
+    cardSaveButton.addEventListener('click', onSaveButtonClick);
+    cardSupportingText.appendChild(cardSaveButton);
     cardWrapper.appendChild(cardSupportingText);
     componentHandler.upgradeElement(cardWrapper);
     sharedMomentsArea.appendChild(cardWrapper);
--- public/sw.js
+++ public/sw.js
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
                         .then(function(res) {
                             return caches.open(CACHE_DYNAMIC_NAME)
                                 .then(function(cache) {
-                                    cache.put(event.request.url, res.clone());
+                                    //cache.put(event.request.url, res.clone());
                                     return res;
                                 });
                         })
