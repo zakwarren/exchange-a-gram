77c8857f9df0a713007f95798c7c452a28cd5b73
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-01-30 20:55:37 +0000

    improve url parsing

--- public/sw.js
+++ public/sw.js
@@ -45,13 +45,15 @@ self.addEventListener('activate', function(event) {
 });
 
 function isInArray(string, array) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] === string) {
-            return true;
-        }
+    var cachePath;
+    if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+        console.log('matched ', string);
+        cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+    } else {
+        cachePath = string; // store the full request (for CDNs)
     }
-    return false;
-}
+    return array.indexOf(cachePath) > -1;
+  }
 
 self.addEventListener('fetch', function(event) {
     var url = 'https://httpbin.org/get';
