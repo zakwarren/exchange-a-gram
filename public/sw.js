b2c281e55de2ec245165f1a5366f61dd2e4ed717
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-01-30 21:22:06 +0000

    comment out trim cache

--- public/sw.js
+++ public/sw.js
@@ -16,20 +16,20 @@ var STATIC_FILES = [
     'https://fonts.googleapis.com/icon?family=Material+Icons',
     'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
-var CACHE_LIMIT = 10;
 
-function trimCache(cacheName, maxItems) {
-    caches.open(cacheName)
-        .then(function(cache) {
-            return cache.keys()
-                .then(function(keys) {
-                    if (keys.length > maxItems) {
-                        cache.delete(keys[0])
-                            .then(trimCache(cacheName, maxItems));
-                    }
-                });
-        });
-}
+// var CACHE_LIMIT = 10;
+// function trimCache(cacheName, maxItems) {
+//     caches.open(cacheName)
+//         .then(function(cache) {
+//             return cache.keys()
+//                 .then(function(keys) {
+//                     if (keys.length > maxItems) {
+//                         cache.delete(keys[0])
+//                             .then(trimCache(cacheName, maxItems));
+//                     }
+//                 });
+//         });
+// }
 
 self.addEventListener('install', function(event) {
     console.log('[Service worker] Installing Service Worker...', event);
@@ -44,7 +44,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
     console.log('[Service worker] Activating Service Worker...', event);
-    trimCache(CACHE_DYNAMIC_NAME, CACHE_LIMIT);
+    // trimCache(CACHE_DYNAMIC_NAME, CACHE_LIMIT);
     event.waitUntil(
         caches.keys()
             .then(function(keyList) {
@@ -83,7 +83,7 @@ self.addEventListener('fetch', function(event) {
                 .then(function(cache) {
                     return fetch(event.request)
                         .then(function(res) {
-                            trimCache(CACHE_DYNAMIC_NAME, CACHE_LIMIT);
+                            // trimCache(CACHE_DYNAMIC_NAME, CACHE_LIMIT);
                             cache.put(event.request, res.clone());
                             return res;
                         });
@@ -114,7 +114,7 @@ self.addEventListener('fetch', function(event) {
                             .then(function(res) {
                                 return caches.open(CACHE_DYNAMIC_NAME)
                                     .then(function(cache) {
-                                        trimCache(CACHE_DYNAMIC_NAME, CACHE_LIMIT);
+                                        // trimCache(CACHE_DYNAMIC_NAME, CACHE_LIMIT);
                                         cache.put(event.request.url, res.clone());
                                         return res;
                                     });
