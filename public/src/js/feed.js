5335f5cc17bb4e2265c8ee5ea0c63a204dbe8240
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-01 16:50:18 +0000

    form basic setup

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,9 @@ var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
 var sharedMomentsArea = document.querySelector('#shared-moments');
+var form = document.querySelector('form');
+var titleInput = document.querySelector('#title');
+var locationInput = document.querySelector('#location');
 
 function openCreatePostModal() {
     createPostArea.style.transform = 'translateY(0)';
@@ -115,3 +118,13 @@ if ('indexedDB' in window) {
             }
         });
 }
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (titleInput.value.trim() === '' || locationInput.value.trim() === '') {
+        return;
+    }
+
+    closeCreatePostModal();
+});
