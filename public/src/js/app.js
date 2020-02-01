39316bfe5608ea0c2dd0a80869f39fe1cb8e1689
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-01 15:48:55 +0000

    responsive image

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,16 +4,16 @@ if (!window.Promise) {
     window.Promise = Promise;
 }
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-            .register('/sw.js')
-            .then(function() {
-                console.log('Service worker registered');
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
-}
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker
+//             .register('/sw.js')
+//             .then(function() {
+//                 console.log('Service worker registered');
+//             })
+//             .catch(function(err) {
+//                 console.log(err);
+//             });
+// }
 
 // function unregisterSW() {
 //     if ('serviceWorker' in navigator) {
