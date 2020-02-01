cbdaf41d40a505a5b4954ae37150329221b5c316
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-01 19:02:03 +0000

    server store post data function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.storePostData = functions.https.onRequest((request, response) => {
+    cors((request, response) => {
+        admin.database().ref('posts').push({
+            id: request.body.id,
+            title: request.body.title,
+            location: request.body.location,
+            image: request.body.image
+        })
+        .then(() => {
+            return response.status(201).json({
+                message: 'Data stored',
+                id: request.body.id
+            });
+        })
+        .catch(err => {
+            return response.status(500).json({
+                error: err
+            });
+        })
+    });
 });
