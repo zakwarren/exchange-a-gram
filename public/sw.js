e252530ba6b8706e78cdd63139ce55996401ae42
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-03 21:20:52 +0000

    listen for push notifications

--- public/sw.js
+++ public/sw.js
@@ -200,3 +200,21 @@ self.addEventListener('notificationclick', event => {
 self.addEventListener('notificationclose', event => {
     console.log('Notification was closed', event);
 });
+
+self.addEventListener('push', event => {
+    console.log('Push notification received', event);
+
+    var data = { title: 'New', content: 'Something new occurred' };
+    if (event.data) {
+        data = JSON.parse(event.data.text());
+    }
+
+    var options = {
+        body: data.content,
+        icon: '/src/images/icons/app-icon-96x96.png',
+        badge: '/src/images/icons/app-icon-96x96.png'
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
