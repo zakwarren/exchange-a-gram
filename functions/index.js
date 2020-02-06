b90e64a59d83c48bd157f73a2c1ba04f20113286
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-06 21:23:14 +0000

    fix to handle firebase changes

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({ origin: true });
 const webpush = require('web-push');
-const formidable = require('formidable');
-const { Storage } = require('@google-cloud/storage');
 const fs = require('fs');
 const UUID = require('uuid-v4');
+const os = require("os");
+const Busboy = require("busboy");
+const path = require('path');
 
 const serviceAccount = require('./key.json');
 const constants = require('./constants');
@@ -14,7 +15,7 @@ const gcConfig = {
     projectId: constants.projectId,
     keyFilename: 'key.json'
 };
-const gcs = new Storage(gcConfig);
+const gcs = require("@google-cloud/storage")(gcConfig);
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -27,18 +28,38 @@ admin.initializeApp({
 exports.storePostData = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
         const uuid = UUID();
-        const formData = new formidable.IncomingForm();
-        formData.parse((request, err, fields, files) => {
-            fs.rename(files.file.path, '/tmp/' + files.file.name);
-            const bucket = gcs.bucket(constants.storage_bucket);
+        const busboy = new Busboy({ headers: request.headers });
+        // These objects will store the values (file + fields) extracted from busboy
+        let upload;
+        const fields = {};
 
-            bucket.upload('/tmp/' + files.file.name, {
-                uploadType: 'media',
+        // This callback will be invoked for each file uploaded
+        busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+            console.log(
+                `File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`
+            );
+            const filepath = path.join(os.tmpdir(), filename);
+            upload = { file: filepath, type: mimetype };
+            file.pipe(fs.createWriteStream(filepath));
+        });
+
+        // This will invoked on every field detected
+        busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+            fields[fieldname] = val;
+        });
+
+        // This callback will be invoked after all uploaded files are saved.
+        busboy.on("finish", () => {
+            var bucket = gcs.bucket("YOUR_PROJECT_ID.appspot.com");
+            bucket.upload(
+            upload.file,
+            {
+                uploadType: "media",
                 metadata: {
-                    metadata: {
-                        contentType: files.file.type,
-                        firebaseStorageDownloadTokens: uuid
-                    }
+                metadata: {
+                    contentType: upload.type,
+                    firebaseStorageDownloadTokens: uuid
+                }
                 }
             }, (err, file) => {
                 if (!err) {
