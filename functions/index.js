24431513beedf581e20754363411d6d8d62a281d
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-09 13:39:52 +0000

    fix server function

--- functions/index.js
+++ functions/index.js
@@ -4,118 +4,137 @@ const cors = require('cors')({ origin: true });
 const webpush = require('web-push');
 const fs = require('fs');
 const UUID = require('uuid-v4');
-const os = require("os");
-const Busboy = require("busboy");
+const os = require('os');
+const Busboy = require('busboy');
 const path = require('path');
 
 const serviceAccount = require('./key.json');
 const constants = require('./constants');
 
-const gcConfig = {
-    projectId: constants.projectId,
-    keyFilename: 'key.json'
-};
-const gcs = require("@google-cloud/storage")(gcConfig);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const serviceAccount = require("./pwagram-fb-key.json");
+
+const gcconfig = {
+  projectId: constants.projectId,
+  keyFilename: 'key.json'
+};
+
+const gcs = require("@google-cloud/storage")(gcconfig);
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: constants.databaseURL
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: constants.databaseURL
 });
 
 exports.storePostData = functions.https.onRequest((request, response) => {
-    cors(request, response, () => {
-        const uuid = UUID();
-        const busboy = new Busboy({ headers: request.headers });
-        // These objects will store the values (file + fields) extracted from busboy
-        let upload;
-        const fields = {};
+  cors(request, response, () => {
+    const uuid = UUID();
+    const busboy = new Busboy({ headers: request.headers });
+    // These objects will store the values (file + fields) extracted from busboy
+    let upload;
+    const fields = {};
 
-        // This callback will be invoked for each file uploaded
-        busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-            console.log(
-                `File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`
-            );
-            const filepath = path.join(os.tmpdir(), filename);
-            upload = { file: filepath, type: mimetype };
-            file.pipe(fs.createWriteStream(filepath));
-        });
-
-        // This will invoked on every field detected
-        busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-            fields[fieldname] = val;
-        });
-
-        // This callback will be invoked after all uploaded files are saved.
-        busboy.on("finish", () => {
-            const bucket = gcs.bucket("YOUR_PROJECT_ID.appspot.com");
-            bucket.upload(
-            upload.file,
-            {
-                uploadType: "media",
-                metadata: {
-                metadata: {
-                    contentType: upload.type,
-                    firebaseStorageDownloadTokens: uuid
-                }
-                }
-            }, (err, file) => {
-                if (!err) {
-
-                    admin.database().ref('posts').push({
-                        id: fields.id,
-                        title: fields.title,
-                        location: fields.location,
-                        image: 'https://firebasestorage.googleapis.com/v0/b/'
-                                    + bucket.name
-                                    + '/o/'
-                                    + encodeURIComponent(file.name)
-                                    + '?alt=media&token='
-                                    + uuid
-                    })
-                    .then(() => {
-                        webpush.setVapidDetails(
-                            'mailto:' + constants.email,
-                            constants.webPushPublicKey,
-                            constants.webPushPrivateKey
-                        );
-                        return admin.database().ref('subscriptions').once('value');
-                    })
-                    .then(subscriptions => {
-                        subscriptions.forEach(sub => {
-                            const pushConfig = {
-                                endpoint: sub.val().endpoint,
-                                keys: {
-                                    auth: sub.val().keys.auth,
-                                    p256dh: sub.val().keys.p256dh
-                                }
-                            };
-            
-                            webpush.sendNotification(pushConfig, JSON.stringify({
-                                title: 'New Post',
-                                content: 'New post created',
-                                openUrl: '/help'
-                            }))
-                            .catch(err => console.log(err));
-                        });
-            
-                        return response.status(201).json({
-                            message: 'Data stored',
-                            id: fields.id
-                        });
-                    })
-                    .catch(err => {
-                        return response.status(500).json({
-                            error: err
-                        });
-                    });
-
-                } else {
-                    console.log(err);
-                }
-            });
-        });
+    // This callback will be invoked for each file uploaded
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+      console.log(
+        `File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`
+      );
+      const filepath = path.join(os.tmpdir(), filename);
+      upload = { file: filepath, type: mimetype };
+      file.pipe(fs.createWriteStream(filepath));
     });
+
+    // This will invoked on every field detected
+    busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
+      fields[fieldname] = val;
+    });
+
+    // This callback will be invoked after all uploaded files are saved.
+    busboy.on("finish", () => {
+      const bucket = gcs.bucket(constants.storage_bucket);
+      bucket.upload(
+        upload.file,
+        {
+          uploadType: "media",
+          metadata: {
+            metadata: {
+              contentType: upload.type,
+              firebaseStorageDownloadTokens: uuid
+            }
+          }
+        },
+        (err, uploadedFile) => {
+          if (!err) {
+            admin
+              .database()
+              .ref('posts')
+              .push({
+                id: fields.id,
+                title: fields.title,
+                location: fields.location,
+                image:
+                  'https://firebasestorage.googleapis.com/v0/b/'
+                  + bucket.name
+                  + '/o/'
+                  + encodeURIComponent(uploadedFile.name)
+                  + '?alt=media&token='
+                  + uuid
+              })
+              .then(() => {
+                webpush.setVapidDetails(
+                  'mailto:' + constants.email,
+                  constants.webPushPublicKey,
+                  constants.webPushPrivateKey
+                );
+                return admin
+                  .database()
+                  .ref('subscriptions')
+                  .once('value');
+              })
+              .then(subscriptions => {
+                subscriptions.forEach(sub => {
+                  const pushConfig = {
+                    endpoint: sub.val().endpoint,
+                    keys: {
+                      auth: sub.val().keys.auth,
+                      p256dh: sub.val().keys.p256dh
+                    }
+                  };
+
+                  webpush
+                    .sendNotification(
+                      pushConfig,
+                      JSON.stringify({
+                        title: 'New Post',
+                        content: 'New post created',
+                        openUrl: '/help'
+                      })
+                    )
+                    .catch(err => console.log(err));
+                });
+                return response
+                  .status(201)
+                  .json({ message: 'Data stored', id: fields.id });
+              })
+              .catch(err => {
+                return response.status(500).json({ error: err });
+              });
+          } else {
+            console.log(err);
+          }
+        }
+      );
+    });
+
+    // The raw bytes of the upload will be in request.rawBody.  Send it to busboy, and get
+    // a callback when it's finished.
+    busboy.end(request.rawBody);
+    // formData.parse(request, function(err, fields, files) {
+    //   fs.rename(files.file.path, "/tmp/" + files.file.name);
+    //   var bucket = gcs.bucket("YOUR_PROJECT_ID.appspot.com");
+    // });
+  });
 });
