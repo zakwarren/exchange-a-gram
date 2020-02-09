53b68334201fde2f12e19d49d92f2d8277e81674
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-09 15:22:01 +0000

    workbox file precaching

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,10 +27,6 @@ const fileManifest = [
     "revision": "2cab47d9e04d664d93c8d91aec59e812"
   },
   {
-    "url": "help/index.html",
-    "revision": "01cbec814afbc7dca28a0ccf03fb6fa0"
-  },
-  {
     "url": "index.html",
     "revision": "f68455f473ffd1dd3035c2e6fea99c21"
   },
@@ -55,88 +51,8 @@ const fileManifest = [
     "revision": "1c6d81b27c9d423bece9869b07a7bd73"
   },
   {
-    "url": "src/images/icons/app-icon-144x144.png",
-    "revision": "83011e228238e66949f0aa0f28f128ef"
-  },
-  {
-    "url": "src/images/icons/app-icon-192x192.png",
-    "revision": "f927cb7f94b4104142dd6e65dcb600c1"
-  },
-  {
-    "url": "src/images/icons/app-icon-256x256.png",
-    "revision": "86c18ed2761e15cd082afb9a86f9093d"
-  },
-  {
-    "url": "src/images/icons/app-icon-384x384.png",
-    "revision": "fbb29bd136322381cc69165fd094ac41"
-  },
-  {
-    "url": "src/images/icons/app-icon-48x48.png",
-    "revision": "45eb5bd6e938c31cb371481b4719eb14"
-  },
-  {
-    "url": "src/images/icons/app-icon-512x512.png",
-    "revision": "d42d62ccce4170072b28e4ae03a8d8d6"
-  },
-  {
-    "url": "src/images/icons/app-icon-96x96.png",
-    "revision": "56420472b13ab9ea107f3b6046b0a824"
-  },
-  {
-    "url": "src/images/icons/apple-icon-114x114.png",
-    "revision": "74061872747d33e4e9f202bdefef8f03"
-  },
-  {
-    "url": "src/images/icons/apple-icon-120x120.png",
-    "revision": "abd1cfb1a51ebe8cddbb9ada65cde578"
-  },
-  {
-    "url": "src/images/icons/apple-icon-144x144.png",
-    "revision": "b4b4f7ced5a981dcd18cb2dc9c2b215a"
-  },
-  {
-    "url": "src/images/icons/apple-icon-152x152.png",
-    "revision": "841f96b69f9f74931d925afb3f64a9c2"
-  },
-  {
-    "url": "src/images/icons/apple-icon-180x180.png",
-    "revision": "2e5e6e6f2685236ab6b0c59b0faebab5"
-  },
-  {
-    "url": "src/images/icons/apple-icon-57x57.png",
-    "revision": "cc93af251fd66d09b099e90bfc0427a8"
-  },
-  {
-    "url": "src/images/icons/apple-icon-60x60.png",
-    "revision": "18b745d372987b94d72febb4d7b3fd70"
-  },
-  {
-    "url": "src/images/icons/apple-icon-72x72.png",
-    "revision": "b650bbe358908a2b217a0087011266b5"
-  },
-  {
-    "url": "src/images/icons/apple-icon-76x76.png",
-    "revision": "bf10706510089815f7bacee1f438291c"
-  },
-  {
-    "url": "src/images/main-image-lg.jpg",
-    "revision": "32f13b2f86d3342f94c04a09e8ee6d2f"
-  },
-  {
-    "url": "src/images/main-image-sm.jpg",
-    "revision": "1429de42428508a9973d1f1f7d9974d3"
-  },
-  {
-    "url": "src/images/main-image.jpg",
-    "revision": "ba6dbc6710bc18c78818dffe78924b6e"
-  },
-  {
-    "url": "src/images/sf-boat.jpg",
-    "revision": "0f282d64b0fb306daf12050e812d6a19"
-  },
-  {
     "url": "src/js/app.js",
-    "revision": "aeed84fc3ad52add5689cade99b20000"
+    "revision": "993fded2bbca804db343b5cdf8dcfc54"
   },
   {
     "url": "src/js/constants.js",
@@ -169,6 +85,22 @@ const fileManifest = [
   {
     "url": "sw.js",
     "revision": "930e242258357dc0d77446cf40fd534c"
+  },
+  {
+    "url": "src/images/main-image-lg.jpg",
+    "revision": "32f13b2f86d3342f94c04a09e8ee6d2f"
+  },
+  {
+    "url": "src/images/main-image-sm.jpg",
+    "revision": "1429de42428508a9973d1f1f7d9974d3"
+  },
+  {
+    "url": "src/images/main-image.jpg",
+    "revision": "ba6dbc6710bc18c78818dffe78924b6e"
+  },
+  {
+    "url": "src/images/sf-boat.jpg",
+    "revision": "0f282d64b0fb306daf12050e812d6a19"
   }
 ];
 
--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   "globDirectory": "public\\",
   "globPatterns": [
-    "**/*.{html,ico,json,css,png,jpg,js}"
+    "**/*.{html,ico,json,css,js}",
+    "src/images/*.{jpg,png}"
   ],
   "swDest": "public/service-worker.js",
   "globIgnores": [
-    "..\\workbox-cli-config.js"
+    "..\\workbox-cli-config.js",
+    "help/**"
   ]
 };
