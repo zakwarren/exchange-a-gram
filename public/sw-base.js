081d416e087fca3bc6db63f6e033216c9eb0052c
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-09 17:04:28 +0000

    background sync and push

--- public/sw-base.js
+++ public/sw-base.js
@@ -44,7 +44,7 @@ workboxSW.router.registerRoute(
     workboxSW.strategies.staleWhileRevalidate({
         cacheName: 'google-fonts',
         cacheExpiration: {
-            maxEntries: 3,
+            maxEntries: 10,
             maxAgeSeconds: 60 * 60 * 24 * 30 // every month
         }
     })
@@ -80,3 +80,95 @@ workboxSW.router.registerRoute(
 );
 
 workboxSW.precache([]);
+
+self.addEventListener('sync', event => {
+    console.log('[Service Worker] Background syncing', event);
+    const storeName = 'sync-posts';
+    if (event.tag === 'sync-new-post') {
+        console.log('[Service Worker] Syncing new posts');
+        event.waitUntil(
+            readAllData(storeName)
+                .then(data => {
+                    for (let dt of data) {
+                        const postData = new FormData();
+                        postData.append('id', dt.id);
+                        postData.append('title', dt.title);
+                        postData.append('location', dt.location);
+                        postData.append('rawLocationLat', dt.rawLocation.lat);
+                        postData.append('rawLocationLng', dt.rawLocation.lng);
+                        postData.append('file', dt.picture, dt.id + '.png');
+
+                        fetch(POST_URL, {
+                            method: 'POST',
+                            body: postData
+                        })
+                        .then(res => {
+                            if (res.ok) {
+                                res.json()
+                                    .then(resData => {
+                                        deleteItemFromData(storeName, resData.id);
+                                    })
+                            }
+                        })
+                        .catch(err => {
+                            console.error('Error while sending data', err);
+                        })
+                    }
+                })
+        );
+    }
+});
+
+self.addEventListener('notificationclick', event => {
+    const notification = event.notification;
+    const action = event.action;
+
+    console.log(notification);
+
+    if (action === 'cancel') {
+        console.log('Cancel was chosen');
+    } else {
+        console.log(action + ' was chosen');
+        event.waitUntil(
+            clients.matchAll()
+                .then(clis => {
+                    const client = clis.find(c => {
+                        return c.visibilityState === 'visible';
+                    });
+                    if (client !== undefined) {
+                        client.navigate(notification.data.url);
+                        client.focus();
+                    } else {
+                        clients.openWindow(notification.data.url);
+                    }
+                })
+        );
+    }
+    notification.close();
+});
+
+self.addEventListener('notificationclose', event => {
+    console.log('Notification was closed', event);
+});
+
+self.addEventListener('push', event => {
+    console.log('Push notification received', event);
+
+    let data = { title: 'New', content: 'Something new occurred', openUrl: '/' };
+    if (event.data) {
+        data = JSON.parse(event.data.text());
+    }
+
+    const options = {
+        body: data.content,
+        icon: '/src/images/icons/app-icon-96x96.png',
+        badge: '/src/images/icons/app-icon-96x96.png',
+        vibrate: [100, 50, 200],
+        data: {
+            url: data.openUrl
+        }
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
