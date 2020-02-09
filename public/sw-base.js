786973c17e55ecb964c35a315099f4600b520e83
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-02-09 16:37:51 +0000

    workbox indexDB

--- public/sw-base.js
+++ public/sw-base.js
@@ -1,6 +1,29 @@
 importScripts('workbox-sw.prod.v2.1.3.js');
+importScripts('/src/js/constants.js');
+importScripts('/src/js/idb.js');
+importScripts('/src/js/utility.js');
 
 const workboxSW = new self.WorkboxSW();
+
+workboxSW.router.registerRoute(
+    DATABASE_URL + 'posts.json',
+    args => {
+        return fetch(args.event.request)
+            .then(res => {
+                const clonedRes = res.clone();
+                clearAllData('posts')
+                    .then(() => {
+                        return clonedRes.json();
+                    })
+                    .then(data => {
+                        for (let key in data) {
+                            writeData('posts', data[key]);
+                        }
+                    });
+                return res;
+            })
+    }
+);
 
 workboxSW.router.registerRoute(
     'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
